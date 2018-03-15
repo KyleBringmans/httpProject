a8a041879f0b959ac7aee14bdc8b9a34e06bfd44
Tomas Geens <tomas.geens@student.kuleuven.be>
2018-03-15 14:03:55 +0100

    image stuff and \r stuff

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,10 @@
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,6 +13,8 @@ import java.net.Socket;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
+
+import javax.imageio.ImageIO;
 
 public class Client {
 	public static void main(String argv[]) throws UnknownHostException, IOException, URISyntaxException{
@@ -22,22 +27,29 @@ public class Client {
 		if(path == null || path.length() == 0){
 			path = "/";
 		}
+		System.out.println(host);
+		System.out.println(path);
 		int port = Integer.parseInt(argv[2]);
+		System.out.println(port);
 		Socket socket = new Socket(host, port);
 		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		out.writeBytes("GET " + path + " HTTP/1.1\n"
-				+ "Host: " + host + "\n\n");
+		System.out.println("GET " + path + " HTTP/1.1\r\n"
+				+ "Host: " + host + "\r\n\r\n");
+		out.writeBytes("GET " + path + " HTTP/1.1\r\n"
+				+ "Host: " + host + "\r\n\r\n");
 		int length = getContentLength(inFromServer);
 		String content = getContent(inFromServer, length);
-		writeOutputToFile(content);
+		writeOutputToFile(content, "response.html");
+
+		writeImageToFile(host, "/tos-jwall.jpg", socket);
 		
 		socket.close();
 	}
 	
 	
-	public static void writeOutputToFile(String content) throws FileNotFoundException{
-		PrintWriter writer = new PrintWriter("response.html");
+	public static void writeOutputToFile(String content, String fileName) throws FileNotFoundException{
+		PrintWriter writer = new PrintWriter(fileName);
 		writer.print(content);
 		writer.close();
 	}
@@ -45,7 +57,10 @@ public class Client {
 	public static int getContentLength(BufferedReader inFromServer) throws IOException{
 		String nextLine = inFromServer.readLine();
 		String header = "Content-Length:";
-		while(!nextLine.startsWith(header))nextLine = inFromServer.readLine();
+		while(!nextLine.startsWith(header)){
+			System.out.println(nextLine);
+			nextLine = inFromServer.readLine();
+		}
 		//remove the text of the header
 		nextLine = nextLine.substring(header.length());
 		//remove the leftover whitespace
@@ -58,14 +73,30 @@ public class Client {
 		String j;
 		j = inFromServer.readLine();
 		while(!j.equals("")){
+			System.out.println(j);
 			j = inFromServer.readLine();
 			continue;
 		}
+		System.out.println(length);
 		StringBuilder response = new StringBuilder();
 		for(int i = 0; i<length; i++) {
-			System.out.print(response.append((char) inFromServer.read()));
+			response.append((char) inFromServer.read());
 
 		}
 		return response.toString();
+	}
+	
+	public static void writeImageToFile(String host, String imagePath, Socket socket) throws IOException{
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		out.writeBytes("GET " + imagePath + " HTTP/1.1\r\n"
+				+ "Host: " + host + "\r\n\r\n");
+		int length = getContentLength(inFromServer);
+		String content = getContent(inFromServer, length);
+		writeOutputToFile(content, "imageInfo.txt");
+		File file= new File("imageInfo.txt");
+		BufferedImage image = ImageIO.read(file);
+		File outputFile= new File("image.jpeg");
+        ImageIO.write(image, "jpeg", outputFile);
 	}
 }
