51b928a45654f9e858482299c25f57329391cbce
Kyle Bringmans <bringmans.kyle@gmail.com>
2018-03-15 08:53:08 +0100

    removed contentLength bug

--- src/Client.java
+++ src/Client.java
@@ -10,17 +10,9 @@ import java.net.Socket;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-
-import com.sun.corba.se.impl.ior.GenericTaggedComponent;
-
-import sun.misc.IOUtils;
 
 public class Client {
 	public static void main(String argv[]) throws UnknownHostException, IOException, URISyntaxException{
-		String sss = "a   b";
-		String ssss = sss.replace(" ", "");
-		System.out.println(ssss);
 		if(argv.length != 3) throw new IOException();
 		String httpCommand = argv[0];
 		String uriString = argv[1];
@@ -37,16 +29,14 @@ public class Client {
 		out.writeBytes("GET " + path + " HTTP/1.1\n"
 				+ "Host: " + host + "\n\n");
 		int length = getContentLength(inFromServer);
-		int[] content = getContent(inFromServer, length);
-		String contentString = new String(content, "CP1250");
-		writeOutputToFile(contentString);
+		String content = getContent(inFromServer, length);
+		writeOutputToFile(content);
 		
 		socket.close();
 	}
 	
 	
 	public static void writeOutputToFile(String content) throws FileNotFoundException{
-		System.out.println("hhier");
 		PrintWriter writer = new PrintWriter("response.html");
 		writer.print(content);
 		writer.close();
@@ -63,21 +53,19 @@ public class Client {
 		return Integer.parseInt(nextLine);
 	}
 	
-	public static int[] getContent(BufferedReader inFromServer, int length) throws IOException{
+	public static String getContent(BufferedReader inFromServer, int length) throws IOException{
 		int[] output = new int[length];
 		String j;
 		j = inFromServer.readLine();
-		while(j.equals("")){
-			System.out.println(j);
+		while(!j.equals("")){
 			j = inFromServer.readLine();
 			continue;
 		}
-		System.out.println("hier");		
-		for(int i = 0; i<length; i++){
-			output[i] = inFromServer.read();
+		StringBuilder response = new StringBuilder();
+		for(int i = 0; i<length; i++) {
+			System.out.print(response.append((char) inFromServer.read()));
+
 		}
-		System.out.println("hsubfqsdkjhfbhjsb");
-		
-		return output;
+		return response.toString();
 	}
 }
